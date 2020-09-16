a66fd220504019f857d79082661c9aec32492f53
Vinicius De Martin Viúde <demartinvinicius@gmail.com>
2020-09-16 18:37:34 -0300

    Adicionar arquivos de projeto.

---
+++ IronNaja/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+using IronNajaUiPathActivities;
+
+namespace IronNaja
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+
+        {
+            Dictionary<string, object> arguments = new Dictionary<string, object>()
+            {
+                { "FileUrl", "teste" }
+            };
+            WorkflowInvoker.Invoke(new IronNajaUiPathActivities.ProcessDocuments(), arguments);
+        }
+    }
+}
---
+++ IronNajaUiPathActivities/ProcessDocuments.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel.Composition;
+using NLog;
+using System.ComponentModel;
+
+namespace IronNajaUiPathActivities
+{
+    public class ProcessDocuments : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> FileUrl { get; set; }
+
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            logger.Info("Starting the activity process");
+
+            string fileurl = FileUrl.Get(context);
+
+            logger.Info($"We're gonna process {fileurl}");
+
+            //throw new NotImplementedException();
+        }
+    }
+}
