c2e9819f325f1228324307252dd77e5ee681ae5f
Vinicius De Martin Viúde <demartinvinicius@gmail.com>
2020-09-17 07:50:25 -0300

    Conexao Dinamica Ok e Refatorada.

---
+++ IronNajaUiPathActivities/ConnectionStringManager.cs
@@ -0,0 +1,55 @@
+﻿using NLog;
+using System.Configuration;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+
+namespace IronNajaUiPathActivities
+{
+    internal class ConnectionStringManager
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void InsertConnectionString(string Server, string DatabaseName, string UserName, string Password)
+        {
+            SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder();
+
+            sqlBuilder.DataSource = Server;
+            sqlBuilder.InitialCatalog = DatabaseName;
+            sqlBuilder.IntegratedSecurity = false;
+            sqlBuilder.UserID = UserName;
+            sqlBuilder.Password = Password;
+            sqlBuilder.MultipleActiveResultSets = true;
+            sqlBuilder.ApplicationName = "EntityFramework";
+            sqlBuilder.Encrypt = true;
+
+            EntityConnectionStringBuilder entityConnection = new EntityConnectionStringBuilder();
+
+            entityConnection.Provider = "System.Data.SqlClient";
+            entityConnection.Metadata = @"res://*/Model1.csdl|res://*/Model1.ssdl|res://*/Model1.msl";
+
+            entityConnection.ProviderConnectionString = sqlBuilder.ToString();
+
+            logger.Info(entityConnection.ConnectionString);
+
+            Configuration config;
+            config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            if (config.GetSection("connectionStrings") != null)
+            {
+                logger.Info("Found config section. It will be deleted");
+                config.Sections.Remove("connectionStrings");
+                config.Save(ConfigurationSaveMode.Full);
+            }
+
+            ConnectionStringSettings connectionStringSettings = new ConnectionStringSettings();
+
+            connectionStringSettings.ConnectionString = entityConnection.ConnectionString;
+            connectionStringSettings.ProviderName = "System.Data.EntityClient";
+            connectionStringSettings.Name = "MouseionEntities";
+            config.ConnectionStrings.ConnectionStrings.Add(connectionStringSettings);
+
+            config.Save(ConfigurationSaveMode.Full);
+            ConfigurationManager.RefreshSection("connectionStrings");
+        }
+    }
+}
--- IronNajaUiPathActivities/ProcessDocuments.cs
+++ IronNajaUiPathActivities/ProcessDocuments.cs
@@ -42,46 +42,12 @@ namespace IronNajaUiPathActivities
         {
             logger.Info("Starting the activity process");
 
-            SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder();
+            ConnectionStringManager connectionStringManager = new ConnectionStringManager();
+            connectionStringManager.InsertConnectionString(ServerName.Get(context),
+                DatabaseName.Get(context),
+                UserName.Get(context),
+                Password.Get(context));
 
-            sqlBuilder.DataSource = ServerName.Get(context);
-            sqlBuilder.InitialCatalog = DatabaseName.Get(context);
-            sqlBuilder.IntegratedSecurity = false;
-            sqlBuilder.UserID = UserName.Get(context);
-            sqlBuilder.Password = Password.Get(context);
-            sqlBuilder.MultipleActiveResultSets = true;
-            sqlBuilder.ApplicationName = "EntityFramework";
-            sqlBuilder.Encrypt = true;
-
-            EntityConnectionStringBuilder entityConnection = new EntityConnectionStringBuilder();
-
-            entityConnection.Provider = "System.Data.SqlClient";
-            entityConnection.Metadata = @"res://*/Model1.csdl|res://*/Model1.ssdl|res://*/Model1.msl";
-
-            entityConnection.ProviderConnectionString = sqlBuilder.ToString();
-
-            logger.Info(entityConnection.ConnectionString);
-
-            Configuration config;
-            config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-
-            if (config.GetSection("connectionStrings") != null)
-            {
-                logger.Info("Found config section. It will be deleted");
-                config.Sections.Remove("connectionStrings");
-                config.Save(ConfigurationSaveMode.Full);
-            }
-
-            ConnectionStringSettings connectionStringSettings = new ConnectionStringSettings();
-            //connectionStringSettings.ConnectionString = "metadata=res://*/Model1.csdl|res://*/Model1.ssdl|res://*/Model1.msl;provider=System.Data.SqlClient;provider connection string=\";data source=mindsforai.database.windows.net;initial catalog=Mouseion;persist security info=True;user id=vinicius;password=M#str@d0;MultipleActiveResultSets=True;App=EntityFramework\"";
-
-            connectionStringSettings.ConnectionString = entityConnection.ConnectionString;
-            connectionStringSettings.ProviderName = "System.Data.EntityClient";
-            connectionStringSettings.Name = "MouseionEntities";
-            config.ConnectionStrings.ConnectionStrings.Add(connectionStringSettings);
-
-            config.Save(ConfigurationSaveMode.Full);
-            ConfigurationManager.RefreshSection("connectionStrings");
             string fileurl = FileUrl.Get(context);
 
             logger.Info($"We're gonna process {fileurl}");
